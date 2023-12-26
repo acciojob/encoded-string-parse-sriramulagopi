cfe69820b94d0e93ed19a8ccf0224c58baa5fcd9
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-26 19:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const parseCode = (str) => {
   // your code here
+    let obj =  { firstName: null, lastName: null, id: null }
+    let str1 = str.split("0");
+    let str2 = [];
+    for (let i=0;i<str1.length;i++){
+        if (str1[i]!==''){
+            str2.push(str1[i]);
+        }
+    }
+    obj.firstName=str2[0];
+    obj.lastName=str2[1];
+    obj.id=str2[2];
+    return obj;
+	
 };
 
 // Do not change the code below
